d50bed2451ad667798cd627c112c7714a4586f60
Quincy <67159014+qu1ncyk@users.noreply.github.com>
2026-02-10 18:43:00 +0100

    fix(nix): switch from nixpkgs unstable to 25.11

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "lazy.nvim with Nix integration";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-25.11";
     flake-utils.url = "github:numtide/flake-utils";
     dream2nix.url = "github:nix-community/dream2nix";
   };
